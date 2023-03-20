4e9861e588391e01d4294ca3dc0b95030d63fbf4
vlsubramaniam <subramaniam.vls@gmail.com>
2023-03-20 20:30:01 +0530

    20-MAR-2023

--- 02-multiple-apps/container/config/webpack.dev.js
+++ 02-multiple-apps/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const {merge} = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJSON = require('../package.json');
@@ -7,24 +6,20 @@ const packageJSON = require('../package.json');
 const devConfig = {
     mode: 'development',
     devServer: {
-        port: 8081,
+        port: 8080,
         historyApiFallback: {
             index: 'index.html'
         }
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'marketing',
-            filename: 'remoteEntry.js',
-            exposes: {
-                './MarketingApp': './src/bootstrap.js'
+            name: 'container',
+            remotes: {
+                marketing: 'marketing@http://localhost:8081/remoteEntry.js'
             },
             shared: packageJSON.dependencies
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
         })
-    ]   
+    ]
 }
 
 module.exports = merge(commonConfig, devConfig);
---
+++ 02-multiple-apps/container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+const App = () => {
+    return (
+        <>
+        <h1>Hi from Container App</h1>
+        <MarketingApp />
+        </>
+    )
+}
+
+export default App;
